42ca4f7b194d8d4f0c35a574f622ea06d1af46c2
Dezen <hello@dezen.me>
2026-07-16 14:00:39 +0700

    📝 sync Formula/tili.rb with v0.1.7's released sha256/version

--- Formula/tili.rb
+++ Formula/tili.rb
@@ -8,17 +8,17 @@
 class Tili < Formula
   desc "i3-like tiling window manager for macOS"
   homepage "https://github.com/itsdezen/tili"
-  version "0.1.6"
+  version "0.1.7"
   license "MIT"
 
   on_arm do
     url "https://github.com/itsdezen/tili/releases/download/v#{version}/tili-#{version}-aarch64-apple-darwin.tar.gz"
-    sha256 "21d49dda763d6500fb9af7fec86f86252a751212529e8a3df527011b31dd251e"
+    sha256 "4f93bb0509220d7f0acb6209dbcef000353f6ec6a194b3e803262703388d8394"
   end
 
   on_intel do
     url "https://github.com/itsdezen/tili/releases/download/v#{version}/tili-#{version}-x86_64-apple-darwin.tar.gz"
-    sha256 "d3d9412b64fa346b60207719977d56c3eedfa765fdc0a3fce5d0721ae61d0bea"
+    sha256 "95da19c8decebdb45e0d8689cdf81ecbcba0d04c7d7489fdec81909990b5c526"
   end
 
   def install
